df57399bae5e684f6e5be0f0f1dfb0ce3f578650
Shanna <s1149901@student.hsleiden.nl>
2024-06-20 15:31:42 +0200

    Request Return staat in order-history maar is nog work in progress

--- src/app/order-history/order-history.component.ts
+++ src/app/order-history/order-history.component.ts
@@ -1,30 +1,37 @@
-import {Component, OnInit} from '@angular/core';
-import {OrderService} from '../services/order.service';
-import {Order} from '../models/order.model';
-import {AuthService} from '../auth/auth.service';
-import {CurrencyPipe} from '@angular/common';
-import {Customer} from '../models/customer.model';
-import {AccountService} from '../services/account.service';
-import {ShippingCostsService} from '../services/shippingcosts.service';
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../services/order.service';
+import { Order } from '../models/order.model';
+import { AuthService } from '../auth/auth.service';
+import { CurrencyPipe } from '@angular/common';
+import { Customer } from '../models/customer.model';
+import { AccountService } from '../services/account.service';
+import { ShippingCostsService } from '../services/shippingcosts.service';
 
 @Component({
+    selector: 'app-order-history',
     standalone: true,
     imports: [CurrencyPipe],
     templateUrl: './order-history.component.html',
-    styleUrl: './order-history.component.scss'
+    styleUrls: ['./order-history.component.scss']
 })
 export class OrderHistoryComponent implements OnInit {
+    customer: Customer;
+    selectedOrder: Order | null = null;
+    reason: string = '';
+    productDetails: string = '';
+    preferredHandling: string = 'refund';
+    showReturnForm: boolean = false;
+    public orderList: Order[];
+    shippingCosts: number;
+
     constructor(
         private orderService: OrderService,
         private authService: AuthService,
         private accountService: AccountService,
         private shippingCostsService: ShippingCostsService
     ) {
+        this.shippingCosts = this.shippingCostsService.getShippingCosts();
     }
-
-    public orderList: Order[];
-    shippingCosts: number = this.shippingCostsService.getShippingCosts();
-    customer: Customer;
 
     ngOnInit() {
         this.orderService
@@ -33,11 +40,41 @@ export class OrderHistoryComponent implements OnInit {
                 this.orderList = orders;
                 console.log(this.orderList);
             });
-        console.log(this.authService.getToken())
+        console.log(this.authService.getToken());
         this.accountService.getCustomer().subscribe((customer: Customer) => {
             this.customer = customer;
             console.log(this.customer);
         });
     }
-}
 
+    openReturnForm(order: Order) {
+        this.selectedOrder = order;
+        this.productDetails = this.selectedOrder.products.map(product => product.name).join(', ');
+        this.reason = '';
+        this.preferredHandling = 'refund';
+        this.showReturnForm = true;
+    }
+
+    closeReturnForm() {
+        this.selectedOrder = null;
+        this.showReturnForm = false;
+    }
+
+    submitReturnForm() {
+        if (this.selectedOrder) {
+            const returnRequest = {
+                orderId: this.selectedOrder.id,
+                reason: this.reason,
+                productDetails: this.productDetails,
+                preferredHandling: this.preferredHandling
+            };
+            console.log('Return request submitted:', returnRequest);
+            this.closeReturnForm();
+            this.showConfirmation();
+        }
+    }
+
+    showConfirmation() {
+        alert('Request submitted, confirmation has been sent to your email');
+    }
+}
